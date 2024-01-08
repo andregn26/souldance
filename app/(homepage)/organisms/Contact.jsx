fba08045b857c867625b24457173d9e5136138f9
andregn26 <andregn26@gmail.com>
2024-01-08 00:51:30 +0000

    added revalidate data

--- app/(homepage)/organisms/Contact.jsx
+++ app/(homepage)/organisms/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
 			whileInView="show"
 			viewport={{ once: true, amount: 0.25 }}
 			id="contactos"
-			className="mt-20">
+			className="mt-20 w-full max-w-screen-lg">
 			<motion.div variants={fadeIn("up", "tween", 0.2, 1)} className="flex w-full flex-col justify-center">
 				<h1 className="text-center font-poppins text-2xl font-semibold md:text-5xl">Fala Connosco</h1>
 				<span className="text-md mt-2 px-2 text-center font-poppins md:mt-4 md:px-5 md:text-xl">
@@ -20,7 +20,7 @@ const Contact = () => {
 				</span>
 			</motion.div>
 			<motion.div variants={fadeIn("up", "tween", 0.2, 1)} className="container mx-auto px-6 py-10">
-				<div className="lg:-mx-6 lg:flex lg:items-center">
+				<div className=" lg:flex lg:items-center">
 					<div className="lg:mx-0 lg:mt-0 lg:flex  lg:flex-col lg:items-center">
 						<div className="mt-6 space-y-8 md:mt-8">
 							<p className="-mx-2 flex items-start">
@@ -131,58 +131,55 @@ const Contact = () => {
 					</div>
 
 					<div className="card mt-8 lg:mx-6 w-full">
-						<div className="card-body mx-auto w-full overflow-hidden rounded-lg px-8 py-10 shadow-xl outline outline-base-content/5 lg:max-w-xl">
-							<h1 className="card-title">What do you want to ask</h1>
+						<div className="card-body mx-auto w-full overflow-hidden rounded-lg px-8 py-10 shadow-xl outline outline-base-content/5 ">
+							<h1 className="card-title">O que gostarias de saber?</h1>
 
 							<form className="mt-6">
 								<div className="flex-1">
 									<label htmlFor="name" className="mb-2 block text-sm">
-										Full Name
+										Nome
 									</label>
 									<input
 										id="name"
 										autoComplete="name"
 										type="text"
-										placeholder="Your Name"
+										placeholder="O teu nome"
 										className="input input-bordered w-full"
 									/>
 								</div>
 
 								<div className="mt-6 flex-1">
 									<label htmlFor="email" className="mb-2 block text-sm">
-										Email address
+										Email
 									</label>
 									<input
 										id="email"
 										autoComplete="email"
 										type="email"
-										placeholder="abcd@example.com"
+										placeholder="abcd@exemplo.com"
 										className="input input-bordered w-full"
 									/>
 								</div>
 
 								<div className="mt-6 w-full">
 									<label htmlFor="message" className="mb-2 block text-sm">
-										Message
+										Mensagem
 									</label>
 									<textarea
 										id="message"
 										autoComplete="message"
 										className="textarea textarea-bordered w-full"
-										placeholder="Message"></textarea>
+										placeholder="Mensagem"></textarea>
 								</div>
 
 								<button className="btn btn-neutral mt-6 w-full transform px-6 py-3 text-sm font-medium capitalize duration-300  ">
-									get in touch
+									Enviar
 								</button>
 							</form>
 						</div>
 					</div>
 				</div>
 			</motion.div>
-			<div className="flex items-center justify-center py-10 font-poppins text-sm lg:text-lg">
-				<h1>© 2024 Souldance Studio. Direitos Reservados. </h1>
-			</div>
 		</motion.section>
 	);
 };
---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="flex items-center justify-center py-4 font-poppins text-sm lg:text-lg">
+			<h1>© 2024 Souldance Studio. Direitos Reservados. </h1>
+		</div>
+	);
+};
+
+export default Footer;
