20b091a32b1392fc64608f6ad36f4427b4984020
andregn26 <andregn26@gmail.com>
2024-01-09 00:21:29 +0000

    added gradient to main

--- app/[modalidades]/page.jsx
+++ app/[modalidades]/page.jsx
@@ -5,13 +5,30 @@ export const revalidate = 60; // revalidate this page every 60 seconds
 
 const Modalities = async () => {
 	const allModalities = await getAllModalities();
-	console.log("🚀 ~ file: page.jsx:6 ~ Modalities ~ allModalities:", allModalities);
+	// console.log("🚀 ~ file: page.jsx:6 ~ Modalities ~ allModalities:", allModalities);
 
 	return (
 		<div className=" flex min-h-screen flex-col items-center justify-between border-2 border-solid border-red-600">
-			{allModalities.map((modality) => (
-				<div key={modality._id}>{modality.name}</div>
-			))}
+			<div className="grid grid-cols-6 gap-4">
+				{allModalities.map((modality) => {
+					console.log("🚀 ~ file: page.jsx:13 ~ {allModalities.map ~ modality:", modality);
+
+					return (
+						<div className="col-span-6 md:col-span-2" key={modality._id}>
+							<h1>{modality.name}</h1>
+							{modality.professors ? (
+								<>
+									{modality.professors.map((professor) => {
+										return <p key={professor._id}>{professor.name} </p>;
+									})}
+								</>
+							) : (
+								<p>No professors</p>
+							)}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
