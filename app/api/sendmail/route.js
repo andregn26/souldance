01a88699729e4d589e8d363abc9a4b3318b10e20
André Gregório <andregn26@gmail.com>
2025-01-19 19:25:09 +0000

    Fix form and add mailersend

--- app/api/sendmail/route.js
+++ app/api/sendmail/route.js
@@ -1,70 +1,61 @@
-import nodemailer from "nodemailer";
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
 	try {
-		const { subject, message, email } = await request.json();
-		console.log("email", subject)
+		const { subject, message, email, name } = await request.json();
+		console.log("🚀 ~ POST ~ name:", name)
+		console.log("🚀 ~ POST ~ email:", email)
+		console.log("🚀 ~ POST ~ message:", message)
+		console.log("🚀 ~ POST ~ subject:", subject)
 
 		if (!subject || !message || !email) {
 			return NextResponse.json({ message: "Todos os campos são obrigatórios!", sent: false }, { status: 400 });
 		}
 
-		let transporter = nodemailer.createTransport({
-			host: "smtp.gmail.com",
-			port: 465,
-			secure: true,
-			auth: {
-				type: "OAuth2", //the type of auth we're using
-				user: "geral.souldance@gmail.com",
-				clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-				clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-				refreshToken: process.env.NEXT_PUBLIC_GOOGLE_REFRESH_TOKEN,
-			},
+		const mailerSend = new MailerSend({
+			apiKey: process.env.MAILERSEND_API_KEY,
 		});
 
-		const emailToSend = {
-			from: '"Souldance - Questões website" <geral.souldance@gmail.com>',
-			to: "geral@souldancestudio.pt",
-			subject: subject,
-			html: `<p>${email} - ${message}</p>`,
-		};
+		const sentFrom = new Sender("info@souldancestudio.pt", name);
 
-		await transporter.sendMail(emailToSend);
+		const recipients = [
+			new Recipient("geral@souldancestudio.pt", "Souldance")
+		];
+
+		const responseTo = [
+			new Recipient(email, name)
+		];
+		const customMessage = `
+		<div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+		  <h2 style="color: #4CAF50; margin-bottom: 16px;">Nova Mensagem Recebida</h2>
+		  <p><strong>Enviado Por:</strong></p>
+		  <ul style="list-style: none; padding: 0;">
+			<li><strong>Email:</strong> ${email}</li>
+			<li><strong>Nome:</strong> ${name}</li>
+		  </ul>
+		  <p><strong>Mensagem:</strong></p>
+		  <div style="margin-top: 12px; padding: 12px; border: 1px solid #ddd; background-color: #f9f9f9; border-radius: 4px;">
+			${message}
+		  </div>
+		  <footer style="margin-top: 24px; font-size: 12px; color: #888;">
+			<p>Esta mensagem foi enviada através do formulário de contacto do site.</p>
+		  </footer>
+		</div>
+	  `;
+	  
+
+		const emailParams = new EmailParams()
+			.setFrom(sentFrom)
+			.setTo(recipients)
+			.setReplyTo(responseTo)
+			.setSubject(subject)
+			.setHtml(customMessage)
+
+		await mailerSend.email.send(emailParams);
 		return NextResponse.json({ message: "email sent", sent: true }, { status: 200 });
 	} catch (error) {
-		return NextResponse.json({ message: "email not sent", sent: false }, { status: 500 });
+		console.error("Error sending email:", error); // Log the full error
+		return NextResponse.json({ message: "email not sent", sent: false, error: error.message }, { status: 500 });
 	}
-
-	// try {
-	// 	const formData = await request.formData();
-	// 	const subject = formData.get("subject");
-	// 	const emailFromClient = formData.get("email");
-	// 	const message = formData.get("message");
-
-	// 	const emailToSend = {
-	// 		from: '"Souldance - Questões website" <geral.souldance@gmail.com>',
-	// 		to: "geral@souldancestudio.pt",
-	// 		subject: subject,
-	// 		html: `<p>${emailFromClient} - ${message}</p>`,
-	// 	};
-
-	// 	let transporter = nodemailer.createTransport({
-	// 		host: "smtp.gmail.com",
-	// 		port: 465,
-	// 		secure: true,
-	// 		auth: {
-	// 			type: "OAuth2", //the type of auth we're using
-	// 			user: "geral.souldance@gmail.com",
-	// 			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-	// 			clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-	// 			refreshToken: process.env.NEXT_PUBLIC_GOOGLE_REFRESH_TOKEN,
-	// 		},
-	// 	});
-
-	// 	await transporter.sendMail(emailToSend);
-	// 	return NextResponse.json({ message: "email sent sucessfully" }, { status: 200 });
-	// } catch (error) {
-	// 	return NextResponse.json({ message: "failed to send email" }, { status: 500 });
-	// }
 }
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,138 +1,134 @@
 "use client";
 
 import { useState } from "react";
-import useContactForm from "@/hooks/useContactForm";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-import sendEmail from "@/utils/sendEmail";
 
 const Form = () => {
-	const [responseMessage, setResponseMessage] = useState({ isSuccessful: null, message: "" });
-	const { values, handleChange } = useContactForm();
+	const [formData, setFormData] = useState({name: "", email: "", subject: "", message: "" });
+	const [isSubmitting, setIsSubmitting] = useState(false);
 
+	// Handle form input changes
+	const handleChange = (e) => {
+		const { id, value } = e.target;
+		setFormData((prev) => ({ ...prev, [id]: value }));
+	};
+
+	// Submit form data
 	const sendMail = async (e) => {
+		e.preventDefault();
+		setIsSubmitting(true); // Disable the button while submitting
+
 		try {
-			e.preventDefault();
-			const response = await axios({
-				method: "post",
-				url: "/api/sendmail",
-				data: {
-					email: values.email,
-					subject: values.subject,
-					message: values.message,
-				},
-			}).then((res) => {
-				if (res.status === 200) {
-					setResponseMessage({ isSuccessful: true, message: "Mensagem enviada!" });
-					const notify = () =>
-						toast.custom((t) => (
-							<div className="toast">
-								<div className="alert alert-success">
-									<span>Mensagem enviada!</span>
-								</div>
-							</div>
-						));
-					notify();
-				}
+			const response = await axios.post("/api/sendmail", formData);
 
-				// console.log(res);
-			});
+			if (response.status === 200) {
+				toast.success("Mensagem enviada!");
+				setFormData({ email: "", subject: "", message: "", name: "" }); // Reset form
+			} else {
+				toast.error("Oops! Alguma coisa correu mal!");
+			}
 		} catch (error) {
-			setResponseMessage({
-				isSuccessful: false,
-				message: "Oops! Alguma coisa correu mal!",
-			});
-			const notify = () =>
-				toast.custom((t) => (
-					<div className="toast">
-						<div className="alert alert-error">
-							<span>Oops! Alguma coisa correu mal!</span>
-						</div>
-					</div>
-				));
-
-			notify();
+			console.error("Error sending email:", error);
+			toast.error("Oops! Alguma coisa correu mal!");
+		} finally {
+			setIsSubmitting(false); // Re-enable the button
 		}
 	};
 
 	return (
 		<div className="card mt-8 lg:mx-6 w-full bg-base-200/60">
-			<div className="card-body mx-auto w-full overflow-hidden rounded-lg px-8 py-10 shadow-xl  ">
+			<div className="card-body mx-auto w-full overflow-hidden rounded-lg px-8 py-10 shadow-xl">
 				<h1 className="card-title">O que gostarias de saber?</h1>
 
 				<form onSubmit={sendMail} className="mt-6">
+					{/* Name Input */}
 					<div className="flex-1">
+						<label htmlFor="name" className="mb-2 block text-sm">
+							Nome
+						</label>
+						<input
+							id="name"
+							value={formData.name}
+							onChange={handleChange}
+							autoComplete="name"
+							type="name"
+							className="input input-bordered w-full"
+							required
+						/>
+					</div>
+					{/* Email Input */}
+					<div className="mt-6 flex-1">
 						<label htmlFor="email" className="mb-2 block text-sm">
 							Email
 						</label>
 						<input
 							id="email"
-							value={values.email}
+							value={formData.email}
 							onChange={handleChange}
 							autoComplete="email"
 							type="email"
 							className="input input-bordered w-full"
+							required
 						/>
 					</div>
 
+					{/* Subject Input */}
 					<div className="mt-6 flex-1">
-						<label htmlFor="name" className="mb-2 block text-sm">
+						<label htmlFor="subject" className="mb-2 block text-sm">
 							Assunto
 						</label>
 						<input
 							id="subject"
-							value={values.subject}
+							value={formData.subject}
 							onChange={handleChange}
 							autoComplete="subject"
 							type="text"
 							className="input input-bordered w-full"
+							required
 						/>
 					</div>
 
+					{/* Message Textarea */}
 					<div className="mt-6 w-full">
 						<label htmlFor="message" className="mb-2 block text-sm">
 							Mensagem
 						</label>
 						<textarea
 							id="message"
-							value={values.message}
+							value={formData.message}
 							onChange={handleChange}
 							autoComplete="message"
-							className="textarea textarea-bordered w-full"></textarea>
+							className="textarea textarea-bordered w-full"
+							required
+						></textarea>
 					</div>
+
+					{/* Submit Button */}
 					<button
 						type="submit"
-						className="btn btn-primary mt-6 w-full transform px-6 py-3 text-sm font-medium capitalize duration-300  ">
-						Enviar
+						className={`btn btn-primary mt-6 w-full transform px-6 py-3 text-sm font-medium capitalize duration-300 ${isSubmitting ? "opacity-50 cursor-not-allowed" : ""
+							}`}
+						disabled={isSubmitting}
+					>
+						{isSubmitting ? "A Enviar..." : "Enviar"}
 					</button>
-					{/* {responseMessage.isSuccessful === null ? (
-						<></>
-					) : (
-						<>
-							{responseMessage.isSuccessful ? (
-								<Toast type="info" message={responseMessage.message} />
-							) : (
-								<Toast type="error" message={responseMessage.message} />
-							)}
-						</>
-					)} */}
+
+					{/* Toast Notifications */}
 					<Toaster
 						position="bottom-right"
 						reverseOrder={false}
-						gutter={8}
-						containerClassName=""
-						containerStyle={{}}
 						toastOptions={{
-							// Define default options
-							className: "bg-primary",
-							duration: 5000,
-
-							// Default options for specific types
 							success: {
-								duration: 3000,
-								theme: {
-									primary: "green",
-									secondary: "black",
+								style: {
+									background: "#4CAF50",
+									color: "#fff",
+								},
+							},
+							error: {
+								style: {
+									background: "#F44336",
+									color: "#fff",
 								},
 							},
 						}}
